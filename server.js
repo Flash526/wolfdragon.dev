28a8397a670122655f3de9782f6d16549a31ee43
Flash <Dom526@gmail.com>
2022-05-05 23:23:19 -0400

    Add SSL

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require('express')
+const https = require('https')
+const fs = require('fs')
 const app = express()
-const port = 3000
+const port = 443
 
 const numberOfPictures = 40;
+
+const server = https.createServer({
+  key: fs.readFileSync('secret/privatekey.pem'),
+  cert: fs.readFileSync('secret/certificate.pem')
+}, app);
 
 app.get('/', (req, res) => {
     res.send(`<img src="images/fish/${Math.ceil(Math.random() * numberOfPictures)}.jpg" style="width: 90vw;" />`)
@@ -10,6 +17,6 @@ app.get('/', (req, res) => {
 
 app.use("/images", express.static(__dirname + '/images'))
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
